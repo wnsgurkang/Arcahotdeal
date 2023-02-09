e386286e2bd4e949d3ba38055769ee5363fdcb2e
wnsgurkang <kjh6586050@gmail.com>
2023-02-09 22:56:48 +0900

    노오호님 지적사항 수정

--- hotdeal.py
+++ hotdeal.py
@@ -11,11 +11,6 @@ def set_chrome_driver():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--lang=ko_KR')
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument('--window-size=1920x1080')
-    chrome_options.add_argument(
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36")
     driver = webdriver.Chrome(service=Service(
         ChromeDriverManager().install()), options=chrome_options)
     return driver
